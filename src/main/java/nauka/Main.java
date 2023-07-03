4e12d146b6efe3120c0e6f53cec31379f3a3b7d6
MajK <majkpacholski@gmail.com>
2023-07-03 19:31:33 +0200

    odpalenie wielu wątków

--- src/main/java/nauka/Main.java
+++ src/main/java/nauka/Main.java
@@ -5,18 +5,13 @@ import java.util.Comparator;
 public class Main {
     public static void main(String[] args) {
 
-        someTest(1, ()-> System.out.println ("to jest liczba jeden"));
-        someTest(2, ()-> System.out.println ("to jest liczba dwa"));
+        Runnable runnable = () ->{
+                String threadName = Thread.currentThread().getName();
+            System.out.println("Hello" + threadName );
+        };
+        runnable.run();
 
-
+    Thread thread = new Thread(runnable);
+    thread.start();
     }
-
-
-    private static void someTest(int number, Runnable action){
-        if(number % 2 ==0){
-            action.run();
-        }
-        }
-
-
 }
